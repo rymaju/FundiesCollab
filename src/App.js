279852354ccf3e26d0dee43af715450315a8b581
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-03-14 18:14:22 -0400

    misc UI fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
-
+import { Helmet } from 'react-helmet'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 import IDE from './components/IDE.component'
@@ -10,13 +10,24 @@ import Home from './components/Home.component'
 
 function App () {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/room/:id' component={IDE} />
-        <Route exact path='*' component={NotFound} />
-      </Switch>
-    </BrowserRouter>
+    <div>
+      <Helmet>
+        <meta charSet='utf-8' />
+        <title>FundiesCollab</title>
+        <meta
+          name='description'
+          content='Collaborative Real-Time coding in Java using the Fundies 2 Tester
+              and Image libraries.'
+        />
+      </Helmet>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/room/:id' component={IDE} />
+          <Route exact path='*' component={NotFound} />
+        </Switch>
+      </BrowserRouter>
+    </div>
   )
 }
 
