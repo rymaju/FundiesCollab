2744d97c63b113a0c651700aa8ed29765d986e82
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-03-19 19:14:37 -0600

    timer fix

--- src_backend/compileRouter.js
+++ src_backend/compileRouter.js
@@ -14,16 +14,17 @@ const histogram = pm2io.histogram({
  * @param {[number, number]} hrStart the start of the timer given by process.hrtime()
  * @returns {void}
  */
-function endTimer (hrstart) {
-  const hsEnd = process.hrtime(hrstart)
-  console.log(`${hsEnd[1] / 100000}ms`)
-  histogram.update(hsEnd[1] / 100000)
+function endTimer (hrStart) {
+  const hsEnd = process.hrtime.bigint()
+  const timeMs = (hsEnd - hrStart) / 1000000
+  console.log(`${timeMs}ms`)
+  histogram.update(timeMs)
 }
 
 router.route('/java').post((req, res) => {
   console.log('new request')
 
-  const hrstart = process.hrtime()
+  const hrStart = process.hrtime.bigint()
   console.log(new Date())
 
   validateInput(req, res)
@@ -36,11 +37,11 @@ router.route('/java').post((req, res) => {
       )
         .then(out => {
           console.log(`Request from room-${input.roomId} took:`)
-          endTimer(hrstart)
+          endTimer(hrStart)
 
           console.log(new Date())
 
-          console.log(process.hrtime(hrstart))
+          console.log(process.hrtime(hrStart))
           if (out === '') {
             res
               .status(400)
@@ -55,14 +56,14 @@ router.route('/java').post((req, res) => {
         })
         .catch(err => {
           console.error(`Error in room ${input.roomId}: ${err}`)
-          endTimer(hrstart)
+          endTimer(hrStart)
 
           res.status(500).end()
         })
     })
     .catch(err => {
       console.error(err)
-      endTimer(hrstart)
+      endTimer(hrStart)
 
       res
         .status(400)
