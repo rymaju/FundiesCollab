230a442e3c7ab6c78880ee534e4f438a2828de8f
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-03-20 05:32:30 +0000

    env fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
+
 module.exports  = {
       name: 'server',
       script: './server.js',
-
       // Options reference: https://pm2.keymetrics.io/docs/usage/application-declaration/
       instances: 'max',
       autorestart: true,
