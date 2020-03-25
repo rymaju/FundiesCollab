6d6da43966833c4a637c683057325689380ae5d0
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-03-25 09:22:10 -0700

    create room fixes

--- src/components/CreateRoom.vue
+++ src/components/CreateRoom.vue
@@ -1,7 +1,7 @@
 <template>
   <b-overlay :show="creatingRoom" rounded="lg" spinner-variant="primary">
     <b-container class="form-container">
-      <b-form class="create-room">
+      <b-form class="create-room" v-on:submit.prevent="createRoom">
         <h2>Create a room</h2>
 
         <b-form-group
@@ -10,7 +10,7 @@
           label-for="input-1"
           description="Your room ID is a unique indentifier that will allow you to share your work just by sharing the link"
         >
-          <b-form-input @change="this.isUniqueRoom = true" v-model="room" :state="validation()" />
+          <b-form-input v-on:input="onChange" v-model="room" :state="validation()" />
           <b-form-invalid-feedback :state="validation()">{{validationErrors}}</b-form-invalid-feedback>
           <b-form-valid-feedback :state="validation()">Looks good!</b-form-valid-feedback>
         </b-form-group>
@@ -52,6 +52,10 @@ export default {
     }
   },
   methods: {
+    onChange() {
+      this.isUniqueRoom = true;
+      console.log("changed");
+    },
     validation() {
       return this.validationErrors.length === 0;
     },
