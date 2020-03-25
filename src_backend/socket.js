f61d0231c0113d03ee2b522c6c4c49ff263258e9
Luca Demian <lucadem1313@gmail.com>
2020-03-25 01:06:30 -0400

    added visualization of other cursors in editor

--- src_backend/socket.js
+++ src_backend/socket.js
@@ -28,6 +28,14 @@ function socketSetup (server) {
     socket.on('send code', data =>
       broadcastCodeHandler(socket, data.room, data.newCode)
     )
+
+    socket.on('send cursor', data => 
+      broadcastCursorHandler(socket, data.room, data.color, data.cursor)
+    )
+
+    socket.on('remove cursor', data => 
+      broadcastRemoveCursorHandler(socket, data.room, data.color)
+    )
   })
 }
 
@@ -43,9 +51,11 @@ async function joinRoomHandler (io, socket, roomId) {
     socket.disconnect(true)
   } else {
     socket.join(roomId)
+    const clientNum = io.sockets.adapter.rooms[roomId].length - 1
+    io.to(`${socket.id}`).emit('new client number', { clientNum })
     const newCode = await roomData.get(roomId).catch(err => console.error(err))
     if (newCode !== undefined) {
-      io.to(`${socket.id}`).emit('sync code', { newCode })
+        io.to(`${socket.id}`).emit('sync code', { newCode })
     }
   }
 }
@@ -69,5 +79,42 @@ function broadcastCodeHandler (socket, roomId, newCode) {
     socket.broadcast.to(roomId).emit('sync code', { newCode })
   }
 }
+
+/**
+ * updates the correct cursor at roomId to the new position, and broadcasts this change to all other users in the room
+ * @param {SocketIO.Socket} socket socket server connection
+ * @param {string} roomId the roomId
+ * @param {string} color the cursor color
+ * @param { line: number, ch: number } cursor the cursor position
+ */
+function broadcastCursorHandler (socket, roomId, color, cursor) {
+  if (isInvalidRoomId(roomId)) {
+    console.error(
+      `attempted broadcast of invalid room: 
+      ${roomId}`
+    )
+    socket.disconnect(true)
+  } else {
+    socket.broadcast.to(roomId).emit('cursor move', { color, cursor })
+  }
+}
+
+/**
+ * updates the correct cursor at roomId to the new position, and broadcasts this change to all other users in the room
+ * @param {SocketIO.Socket} socket socket server connection
+ * @param {string} roomId the roomId
+ * @param {string} color the cursor color
+ */
+function broadcastRemoveCursorHandler (socket, roomId, color) {
+    if (isInvalidRoomId(roomId)) {
+      console.error(
+        `attempted broadcast of invalid room: 
+        ${roomId}`
+      )
+      socket.disconnect(true)
+    } else {
+      socket.broadcast.to(roomId).emit('remove cursor', { color })
+    }
+  }
 
 module.exports = socketSetup
