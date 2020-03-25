2c56cae04479a815b508f8d7045c4f6b896d8a4c
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-03-25 01:43:57 -0700

    lazy loading all components

--- src/main.js
+++ src/main.js
@@ -6,10 +6,6 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import VueRouter from 'vue-router'
 import VueClipboard from 'vue-clipboard2'
 
-import CreateRoom from './components/CreateRoom'
-import Home from './components/Home'
-import NotFound from './components/NotFound'
-
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
 Vue.use(VueRouter)
@@ -20,10 +16,10 @@ Vue.config.productionTip = false
 const router = new VueRouter({
   mode: 'history',
   routes: [
-    { path: '/', component: Home },
-    { path: '/room', component: CreateRoom },
+    { path: '/', component: () => import('./components/Home') },
+    { path: '/room', component: () => import('./components/CreateRoom') },
     { path: '/room/:id', component: () => import('./components/Room') },
-    { path: '*', component: NotFound }
+    { path: '*', component: () => import('./components/NotFound') }
   ]
 })
 
